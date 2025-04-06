781b527e2f9b3af64542fb1db9fe39a9f22ba6f2
Abhishek Soren <abhisheksoren95@gmail.com>
2025-04-06 14:41:36 +0530

    .

--- 00-Practice/coding_challenge_2/script/script.js
+++ 00-Practice/coding_challenge_2/script/script.js
@@ -3,35 +3,41 @@
 let markWeight = 78;
 let markHeight = 1.69;
 
-let markBMI = markWeight / markHeight ** 2;
+let markBMI = (markWeight / markHeight ** 2).toFixed(1);
 
 let johnWeight = 92;
 let johnHeight = 1.95;
 
-let johnBMI = johnWeight / johnHeight ** 2;
+let johnBMI = (johnWeight / johnHeight ** 2).toFixed(1);
 
 let markHigherBMI = markBMI > johnBMI;
 
 console.log(`Mark: Weight=${markWeight}kg, Height=${markHeight}m, BMI=${markBMI}`);
 console.log(`John: Weight=${johnWeight}kg, Height=${johnHeight}m, BMI=${johnBMI}`);
 
-console.log("For Data 1 : Is Marks BMI higher? ", markHigherBMI);
+if (markBMI > johnBMI) {
+    console.log(`Mark's BMI ${markBMI} is higher than John's BMI ${johnBMI} !`);
+} else {
+    console.log(`John's BMI ${johnBMI} is higher than Mark's BMI ${markBMI} !`);
+}
 
 // Data 2
 
 markWeight = 95;
 markHeight = 1.88;
 
-markBMI = markWeight / markHeight ** 2;
+markBMI = (markWeight / markHeight ** 2).toFixed(1);
 
 johnWeight = 85;
 johnHeight = 1.76;
 
-johnBMI = johnWeight / johnHeight ** 2;
-
-markHigherBMI = markBMI > johnBMI;
+johnBMI = (johnWeight / johnHeight ** 2).toFixed(1);
 
 console.log(`Mark: Weight=${markWeight}kg, Height=${markHeight}m, BMI=${markBMI}`);
 console.log(`John: Weight=${johnWeight}kg, Height=${johnHeight}m, BMI=${johnBMI}`);
 
-console.log("For Data 2 : Is Marks BMI higher? ", markHigherBMI);
+if (markBMI > johnBMI) {
+    console.log(`Mark's BMI ${markBMI} is higher than John's BMI ${johnBMI} !`);
+} else {
+    console.log(`John's BMI ${johnBMI} is higher than Mark's BMI ${markBMI} !`);
+}
