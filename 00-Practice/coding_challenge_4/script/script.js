af6425f6d1e9cc03e0caffbbf4ec87896d3f040d
Abhishek Soren <abhisheksoren95@gmail.com>
2025-04-08 14:51:31 +0530

    .

--- 00-Practice/coding_challenge_4/script/script.js
+++ 00-Practice/coding_challenge_4/script/script.js
@@ -1,37 +1,14 @@
-// Data 1
+let bills = [275, 40, 430];
 
-let markWeight = 78;
-let markHeight = 1.69;
+function calcTotal(bill) {
+    let tip = bill >= 50 && bill < 300 ? 0.15 : 0.2;
+    console.log(
+        `The bill was ${bill} the tip was ${bill * tip}, and the total value ${bill + bill * tip}.`
+    );
+}
 
-let markBMI = markWeight / markHeight ** 2;
-
-let johnWeight = 92;
-let johnHeight = 1.95;
-
-let johnBMI = johnWeight / johnHeight ** 2;
-
-let markHigherBMI = markBMI > johnBMI;
-
-console.log(`Mark: Weight=${markWeight}kg, Height=${markHeight}m, BMI=${markBMI}`);
-console.log(`John: Weight=${johnWeight}kg, Height=${johnHeight}m, BMI=${johnBMI}`);
-
-console.log("For Data 1 : Is Marks BMI higher? ", markHigherBMI);
-
-// Data 2
-
-markWeight = 95;
-markHeight = 1.88;
-
-markBMI = markWeight / markHeight ** 2;
-
-johnWeight = 85;
-johnHeight = 1.76;
-
-johnBMI = johnWeight / johnHeight ** 2;
-
-markHigherBMI = markBMI > johnBMI;
-
-console.log(`Mark: Weight=${markWeight}kg, Height=${markHeight}m, BMI=${markBMI}`);
-console.log(`John: Weight=${johnWeight}kg, Height=${johnHeight}m, BMI=${johnBMI}`);
-
-console.log("For Data 2 : Is Marks BMI higher? ", markHigherBMI);
+let i = 0;
+for (const element of bills) {
+    console.log(`For Data ${++i} : `);
+    calcTotal(element);
+}
